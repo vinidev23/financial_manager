f4031cc880a4afd2b66507a0609d9f33c7815e41
vinidev23 <vinidev23>
2025-09-06 14:29:18 -0300

    feat: Adicionando rota de usuário do Banco de Dados

--- src/server.js
+++ src/server.js
@@ -15,3 +15,24 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Servidor na porta ${PORT}`);
 });
+
+app.post('/api/users', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const checkUser = await db.query("SELECT * FROM users WHERE email = $1", [`${email}`]);
+        if (checkUser.rows.length > 0) {
+            return res.status(409).json({ message: 'E-mail já Cadastrado.' });
+        }
+
+        const result = await db.query(
+            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [name, email, password]
+        );
+
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error("Erro ao registrar usuário", error);
+        res.status(500).json({ message: "Erro interno do servidor." });
+    }
+});
